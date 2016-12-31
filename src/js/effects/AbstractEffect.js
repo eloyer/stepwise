d488fa120e311c5914781c0d31b2191a6861002c
Erik Loyer <erikcloyer@gmail.com>
2016-12-31 06:34:42 -0800

    Add support for text display of temporal, environmental, geographic metadata, for display of character names in multi-character scripts, and options for enabling/disabling those features

--- src/js/effects/AbstractEffect.js
+++ src/js/effects/AbstractEffect.js
@@ -14,8 +14,17 @@
 
     $.extend(true, $.fn.stepwise.effects, extensionMethods);
 
-    function AbstractEffect() {
+    function AbstractEffect(options) {
+        this.options = {
+            useCharacterNames: true,
+            createBreakTags: true,
+            includeTemporal: true,
+            includeEnvironmental: true,
+            includeGeographic: true
+        };
+        $.extend(this.options, options);
         this.bindings = [];
+        this.lastCharacter = null;
     }
 
     AbstractEffect.prototype.bindings = null;
@@ -23,20 +32,106 @@
     AbstractEffect.prototype.bindToInstance = function(stepwiseInstance) {
     	var me = this;
     	this.instance = stepwiseInstance;
-    	$(this.instance.element).bind("executeStep", function(event, step) {
+        this.visibleCharacterCount = this.getVisibleCharacterCount();
+        $(this.instance.element).bind("executeStep", function(event, step) {
             var bindings = me.eligibleBindingsForStep(step);
             for (var i in bindings) {
                 switch (step.command) {
 
                     case "speak":
                     case "narrate":
-                    me.displayStep(step, bindings[i].element);
+                    case "think":
+                    me.displayStep(step, bindings[i].element, me.parseCharacterAction(step));
+                    break;
+
+                    case "setlocation":
+                    if (me.options.includeGeographic) {
+                        me.displayStep(step, bindings[i].element, '<br />' + step.target.name);
+                    }
+                    break;
+
+                    case "settemperature":
+                    if (me.options.includeEnvironmental) {
+                       me.displayStep(step, bindings[i].element, '<br />' + me.parseTemperature(step));
+                    }
+                    break;
+
+                    case "setweather":
+                    // do nothing
+                    break;
+
+                    case "setdate":
+                    if (me.options.includeTemporal) {
+                        me.displayStep(step, bindings[i].element, '<br />' + step.date.toDateString());
+                    }
+                    break;
+
+                    case "settime":
+                    if (me.options.includeTemporal) {
+                        me.displayStep(step, bindings[i].element, '<br />' + step.date.toLocaleTimeString());
+                    }
                     break;
 
                 }                
             }
 
 	    });
+    }
+
+    AbstractEffect.prototype.getVisibleCharacterCount = function() {
+        var count = 0;
+        var i, n = this.instance.score.characters.length;
+        for (i=0; i<n; i++) {
+            if (this.instance.score.characters[i].visible) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    AbstractEffect.prototype.parseCharacterAction = function(step) {
+        var text = step.content;
+        if (this.options.useCharacterNames && (this.visibleCharacterCount > 1)) {
+            var currentCharacter = step.target;
+            if (currentCharacter.visible) {
+                var isNewCharacter = (currentCharacter != this.lastCharacter);
+                if (isNewCharacter) {
+                    switch (step.command) {
+
+                        case 'speak':
+                        case 'narrate':
+                        text = '<br />' + currentCharacter.fullName.toUpperCase() + ": " + text;
+                        break;
+
+                        case 'think':
+                        text = '<br />' + currentCharacter.fullName.toUpperCase() + ": [" + text + "]";
+                        break;
+
+                    }
+                    this.lastCharacter = currentCharacter;
+                }
+            }
+        }
+        if (this.options.createBreakTags) {
+            text = text.replace(/(?:\\r\\n|\\r|\\n)/g, '<br />');
+        }
+        return text;
+    }
+
+    AbstractEffect.prototype.parseTemperature = function(step) {
+        var text = step.content + "°";
+        switch (step.units) {
+            
+            case TemperatureUnits.CELSIUS:
+            text = '<br />' + text + "C";
+            break;
+                
+            case TemperatureUnits.FAHRENHEIT:
+            text = '<br />' + text + "F";
+            break;
+            
+        }
+        return text;
     }
 
     AbstractEffect.prototype.bindToElement = function(element) {
@@ -89,7 +184,7 @@
         return eligibleBindings;
     }
 
-    AbstractEffect.prototype.displayStep = function(step, element) {
+    AbstractEffect.prototype.displayStep = function(step, element, processedContent) {
     	var okToDisplay = true;
     	if (step.target != null) {
     		okToDisplay = step.target.visible;
@@ -98,7 +193,7 @@
 	    	if (!step.append) {
 	    		$(element).empty();
 	    	}
-	    	$(element).append(content);
+	    	$(element).append(processedContent);
     	}
     }
 
