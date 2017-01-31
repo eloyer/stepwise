204d9becb2e5156e35ebd774b3397db8d66f55c8
Erik Loyer <erikcloyer@gmail.com>
2017-01-31 07:06:59 -0800

    Refactor Slabtext effect

--- src/js/effects/Slabtext.js
+++ src/js/effects/Slabtext.js
@@ -7,63 +7,60 @@
 (function($) {
 
     var extensionMethods = {
-
-	 	Slabtext: new Slabtext()
-
+	 	Slabtext: Slabtext
     };
 
     $.extend(true, $.fn.stepwise.effects, extensionMethods);
 
-    function Slabtext() {
+    function Slabtext(instance, options) {
+        $.fn.stepwise.effects.AbstractEffect.call(this, instance, options);
     	this.contentBuffer = "";
+        var localOptions = {
+            clearHeight: -1,
+            fontRatio: .78,
+            maxFontSize: 999,
+            minCharsPerLine: 5,
+            viewportBreakpoint: 600
+        };
+        $.extend(this.options, localOptions);
+        $.extend(this.options, options);
     }
 
-    Slabtext.prototype.bindToInstance = function(stepwiseInstance) {
-    	var me = this;
-    	this.instance = stepwiseInstance;
-    	$(this.instance.element).bind("executeStep", function(event, step) {
-    		switch (step.command) {
+    Slabtext.prototype = Object.create($.fn.stepwise.effects.AbstractEffect.prototype, {
 
-    			case "speak":
-    			case "narrate":
-	    		if (me.element != null) {
-	    			if (me.character != null) {
-	    				if (step.target.id == me.character.id) {
-	    					me.displayStep(step);
-	    				}
-	    			} else {
-	    				me.displayStep(step);
-	    			}
-	    		}
-    			break;
+        displayStep: {
+            value: function(step, element, processedContent) {
+                var okToDisplay = true;
+                if ((step.target != null) && (step.target.visible != null)) {
+                    okToDisplay = step.target.visible;
+                }
+                if (okToDisplay) {
+                    if (!step.append && !this.options.appendOnly) {
+                        this.contentBuffer = "";
+                    }
+                    if (this.options.clearHeight != -1) {
+                        if ($(element).height() > this.options.clearHeight) {
+                            this.contentBuffer = "";
+                        }   
+                    }
+                    this.contentBuffer += processedContent;
+                    $(element).text(this.contentBuffer);
+                    $(element).slabText({
+                        fontRatio: this.options.fontRatio,
+                        maxFontSize: this.options.maxFontSize,
+                        minCharsPerLine: this.options.minCharsPerLine,
+                        viewportBreakpoint: this.options.viewportBreakpoint
+                    });
+                    this.callDisplayStepHandlers(step, element);
+                }
+            },
+            enumerable: true,
+            configurable: true,
+            writable: true
+        }
 
-    		}
-	    });
-    }
+    });
 
-    Slabtext.prototype.bindToElement = function(element) {
-    	this.element = element;
-    }
-
-    Slabtext.prototype.bindToCharacter = function(character) {
-    	this.character = character;
-    }
-
-    Slabtext.prototype.displayStep = function(step) {
-    	var okToDisplay = true;
-    	if (step.target != null) {
-    		okToDisplay = step.target.visible;
-    	}
-    	if (okToDisplay) {
-	    	if (!step.append) {
-	    		this.contentBuffer = "";
-	    	}
-	    	this.contentBuffer += step.content;
-	    	$(this.element).text(this.contentBuffer);
-			$(this.element).slabText({
-				viewportBreakpoint: 600
-			});
-    	}
-    }
+    Slabtext.prototype.constructor = Slabtext;
 
 })(jQuery);
